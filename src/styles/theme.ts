8428ce04062dbc75d7df9a4d8d46afad2ca26aff
kaue guerra <kaueguerra@gmail.com>
2021-11-28 15:44:57 -0300

    config font default and created _document.tsx

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+    fonts: {
+        headings: 'Roboto',
+        body: 'Roboto'
+    },
     styles: {
         global: {
             body: {
