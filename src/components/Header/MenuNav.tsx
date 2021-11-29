d70427939f167e0271e2a4a7aeb93350ed0ef8ba
kaue guerra <kaueguerra@gmail.com>
2021-11-28 23:35:04 -0300

    created Active link component

--- src/components/Header/MenuNav.tsx
+++ src/components/Header/MenuNav.tsx
@@ -1,14 +1,15 @@
 import { Flex, HStack, Icon, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import { RiLogoutBoxLine } from 'react-icons/ri'
+import { ActiveLink } from '../ActiveLink'
 
 export function MenuNav() {
     return (
         <Flex align="center" ml="auto">
-            <HStack spacing="4">
-                <Link href="/profile">
+            <HStack spacing={["2", "4"]}>
+                <ActiveLink href="/profile">
                     <Text _hover={{ color: 'gray.500' }} cursor="pointer">Meu Perfil</Text>
-                </Link>
+                </ActiveLink>
                 <Link href="/sair">
                     <Icon as={RiLogoutBoxLine} _hover={{ color: 'gray.500' }} cursor="pointer" />
                 </Link>
